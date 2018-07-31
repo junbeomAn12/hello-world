f71fca8ed05660d869acce8d660799cddb104c60
Junbeom_An <wnsqja8744@gmail.com>
2018-07-31 15:10:17 +0900

    hi code 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello code");	
+		System.out.println("Hello code");
+		System.out.println("hi code");
 	}
 }
