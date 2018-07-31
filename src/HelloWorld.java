b349bf536fbfc07f1717cc63433f44b1496b88e0
Junbeom_An <wnsqja8744@gmail.com>
2018-07-31 15:12:20 +0900

    hello world 두개 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello code");
 		System.out.println("hi code");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 }
